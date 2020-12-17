83391779384eeac6862bbe57dfa534f59bb26d43
serphico <gomez.ecm@gmail.com>
2020-12-17 17:46:40 -0300

    ItemCount terminado

--- mundo-amigurumi/src/components/ItemCount/ItemCount.js
+++ mundo-amigurumi/src/components/ItemCount/ItemCount.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
+import './ItemCount.scss'
 
 const ItemCount = (props) => {
 
@@ -9,24 +10,26 @@ const ItemCount = (props) => {
 
     const [count, setCount] = React.useState(initial);
 
-
     const countAdd = () => {
-           if(count < stock){
+           if(count < stock && count > 0){
             setCount(count + 1 );
            }
     }
 
     const countRemove = () => {
 
-        if(count > initial){
+        if(count > initial && count > 0){
             setCount(            
                 count - 1            
                 );
         }
     }
 
+
+
+
     return(
-        <div>
+        <div className="countContainer">
             <RemoveIcon onClick={countRemove}></RemoveIcon>
                 <span>{count}</span>
             <AddIcon onClick={countAdd}></AddIcon>
--- mundo-amigurumi/src/components/ItemListContainer/ItemListContainer.js
+++ mundo-amigurumi/src/components/ItemListContainer/ItemListContainer.js
@@ -7,7 +7,7 @@ const ItemListContainer = (props) => {
     return(
         <section>
             <h1>{props.greeting}</h1>
-            <ItemCount stock='5' initial='1'/>
+            <ItemCount stock='5' initial='0'/>
             <Button variant="contained" color="primary" >
                  Hello World
             </Button>
